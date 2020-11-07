8f660b58e668f40cdf1fcc30a04f3c8bf88d44ae
tomtom-code <tomlai8882000@hotmail.com>
2020-11-06 19:30:55 -0600

    set up

--- backend/app/controllers/item_controller.rb
+++ backend/app/controllers/item_controller.rb
@@ -1,2 +1,14 @@
 class ItemController < ApplicationController
+
+    def index
+        items = Item.all
+
+        render json: items
+    end 
+
+    def show
+        item = Item.find(params[:id])
+
+        render json: item
+    end
 end
